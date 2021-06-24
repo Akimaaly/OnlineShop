9a43c54f0576df4c6306e31ea0cf8ddc0fb17629
Mikhail.CallInNickOff <mihalka999@gmail.com>
2021-06-24 20:55:10 +0300

    Навигация на CardList

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,44 @@
-import { Layout, Row, Col } from 'antd';
-import Header from './Components/Header/Header';
-import Sider from './Components/Sider/Sider';
-import CardsList from './Components/CardsList/CardsList';
-import 'antd/dist/antd.css';
-import CategoriesNavBar from './Components/Categories/CategoriesNavBar';
+import { Layout, Row, Col } from "antd";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import Sider from "./Components/Sider/Sider";
+import CardsList from "./Components/CardsList/CardsList";
+import "antd/dist/antd.css";
+import CategoriesNavBar from "./Components/Categories/CategoriesNavBar";
 
 const { Content, Footer } = Layout;
 
 function App() {
   return (
-    <div>
-      <Layout>
-        <Header />
+    <BrowserRouter>
+      <div>
         <Layout>
-          <Row>
-            <Col flex='300px'>
-              <CategoriesNavBar />
-            </Col>
-            <Col flex='auto'>
-              <Content>
-                <CardsList />
-              </Content>
-            </Col>
-          </Row>
+          <Header />
+          <Layout>
+            <Row>
+              <Col flex="300px">
+                <CategoriesNavBar />
+              </Col>
+              <Col flex="auto">
+                <Content>
+                  <CardsList />
+                </Content>
+              </Col>
+            </Row>
+          </Layout>
+          <Footer>Footer</Footer>
         </Layout>
-        <Footer>Footer</Footer>
-      </Layout>
-    </div>
+      </div>
+      <Switch>
+      <Route path="/cardlist:name">
+        <CardsList />
+      </Route>
+      <Route path="/cardlist">
+        <CardsList />
+      </Route>
+
+      </Switch>
+    </BrowserRouter>
   );
 }
 
