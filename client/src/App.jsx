953f6fd7256579208d7b189b1907478bf4f66625
S.Andronova <andronova1995@yandex.ru>
2021-06-25 10:21:37 +0300

    Reg

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
+import React, { useState } from 'react';
 
+import Registration from './Components/Registration/Registration';
+import Login from './Components/Login/Login';
+import 'antd/dist/antd.css';
 
 function App() {
   return (
-    <div>
-     Привет!
-    </div>
+    <>
+      <Registration />
+    </>
   );
 }
 
